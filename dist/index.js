a8492b8c91952cc7199dfda7b6bb1d7d515c7ea9
zbenseghier <35616124+zbenseghier@users.noreply.github.com>
2023-08-19 20:30:31 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 
 'use strict'
 
-if (process.env.APP_ENV === 'production') {
+if (process.env.APP_ENV === 'production'){
   module.exports = require('./chargily-epay-react-js.cjs.production.min.js')
 } else {
   module.exports = require('./chargily-epay-react-js.cjs.development.js')
